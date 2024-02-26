958084178e5b780e0cee7311425fef63b7485577
hadarTheColl1234 <syrlysyr8@gmail.com>
2024-02-26 21:01:31 +0200

    start movment

---
+++ HadarYonini/Assets/MovmentScrypt.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmentScrypt : MonoBehaviour
+{
+    private Transform transform1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform1 = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
